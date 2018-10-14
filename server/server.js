de7d9c0c5689abc17f77e53bf8db8c9d2b992538
TheNech <savchenkov.vk@yandex.ru>
2018-10-14 11:06:51 +0300

    add post-route for /api/events

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 8000;
 
@@ -6,6 +8,9 @@ const startDate = new Date();
 const data = require('./data/events.json');
 const { getFormatedTime, isTypeCorrect, filterDataByType } = require('./app/functions');
 const { allowTypes } = require('./config');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, (err) => {
     if (err) {
@@ -25,6 +30,21 @@ app.get('/status', (req, res) => {
 app.get('/api/events', (req, res) => {
     if (req.query.type) {
         let type = req.query.type.split(':');
+        if (!isTypeCorrect(type)) {
+            res.sendStatus(400);
+        } else if (type.length === allowTypes.length) {
+            res.json(data);
+        } else {
+            res.json(filterDataByType(data, type));
+        }
+    } else {
+        res.json(data);
+    }
+});
+
+app.post('/api/events', (req, res) => {
+    if(req.body.type) {
+        let type = req.body.type.split(':');
         if (!isTypeCorrect(type)) {
             res.sendStatus(400);
         } else if (type.length === allowTypes.length) {
