71b14e669f18da0599d5fb6561d8e74f37f1eb49
TheNech <savchenkov.vk@yandex.ru>
2018-11-06 16:04:35 +0300

    удаление подмодуля

--- scripts/flux/Actions.js
+++ scripts/flux/Actions.js
@@ -1,4 +1,4 @@
-import Dispatcher from './Dispatcher';
+import Dispatcher from './VideoDispatcher';
 
 class Actions {
     /**
--- scripts/flux/VideoStore.js
+++ scripts/flux/VideoStore.js
@@ -1,11 +1,16 @@
 import { Store } from '../shri-2018-flux/js/Store';
-import dispatcher from './Dispatcher';
+import dispatcher from './VideoDispatcher';
 
 class VideoStore extends Store {
     constructor(dispatcher) {
-        super({}, dispatcher);
+        super(dispatcher);
+        this.state = {};
     }
 
+    /**
+     * Callback for dispetcher
+     * @param {Object} action 
+     */
     actionToRegister(action) {
         switch(action.type) {
             case 'SET_BRIGHT': {
@@ -27,7 +32,7 @@ class VideoStore extends Store {
                     this.state[action.data.video] = {};
                     this.state[action.data.video].contrast = action.data.value;
                 }
-                this.notify(this.state);
+                this.notify();
 
                 console.log('Store was changed');
                 break;
